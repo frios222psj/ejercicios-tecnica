56b646303a959cbfe3dd54c3d8d01a9238902307
Fernando Luis Rios <frios222psj@gmail.com>
2017-03-25 05:15:15 -0300

    Add files via upload

---
+++ ejercicio_18.py
@@ -0,0 +1,13 @@
+#Crear un programa que pinte un tablero de ajedrez, los peones con la letra P,
+#las torres con T, los caballos con C, los alfiles con A, el rey con R y la reina con M.
+
+
+print ("T C A R M A C T ")
+print ("P P P P P P P P ")
+print ("██  ██  ██  ██  ")
+print ("  ██  ██  ██  ██")
+print ("██  ██  ██  ██  ")
+print ("  ██  ██  ██  ██")
+print ("P P P P P P P P ")
+print ("T C A R M A C T")
+
