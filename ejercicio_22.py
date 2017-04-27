19f3271242623df94766832eeda004ffc844a456
Fernando Luis Rios <frios222psj@gmail.com>
2017-04-27 03:57:15 -0300

    ejercicio22.py

---
+++ ejercicio_22.py
@@ -0,0 +1,8 @@
+# Crear una función que permita filtrar una lista de correos teniendo como comparación una
+#lista de nombres de persona, la función debe determinar cuales correos tienen como nombre de
+# usuario un nombre de persona y devolver una lista de esos correos.
+
+
+
+
+
