4107e1124c615d5c0f753e40735c2ce1d664ab52
Fernando Luis Rios <frios222psj@gmail.com>
2017-04-27 03:58:25 -0300

    ejercicio_24.py

---
+++ ejercicio_24.py
@@ -0,0 +1,5 @@
+#Crear un programa que permita cargar tareas a realizar, luego que permita mostrar
+#el listado completo de tareas en la pantalla. Adicionalmente que permita marcar
+# una tarea como oculta, y tambien desocultar tareas. Para ocultar claramente
+#debera mostrar las tareas que estan visibles, y para desocultar,
+#se debera poder elegir de entre las que esten ocultas
